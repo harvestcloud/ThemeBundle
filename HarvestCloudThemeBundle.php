44a8ccd6328fa21a16730bccfd2f2e156583dbbd
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2014-03-30 15:43:33 -0400

    Adding HarvestCloudThemeBundle and initial styles

---
+++ HarvestCloudThemeBundle.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Harvest Cloud package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HarvestCloud\ThemeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * HarvestCloudThemeBundle
+ *
+ * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+ * @since  2014-03-30
+ */
+class HarvestCloudThemeBundle extends Bundle
+{
+}
